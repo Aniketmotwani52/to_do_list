b94f65244e2f9316136463e673fabe354ea54d43
Aniket <aniketmotwani52@gmail.com>
2023-01-09 21:15:24 +0530

    commit to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App()
+{
+  
+  const [newTask, setNewTask] = useState("");
+  const [tasks, addNewTask] = useState([]);
+  
+  function handleNewTask(event)
+  {
+    console.log(event.target.value);
+    const newValue = event.target.value;
+    setNewTask(newValue);
+  }
+
+  function handleList()
+  {
+    addNewTask((prevTasks) => {return[...prevTasks,newTask]});
+    setNewTask("");
+  }
+
+  return(
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" placeholder="Add your Task" onChange={handleNewTask} value={newTask}></input>
+        <button onClick={handleList}>
+          <span> Add </span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {tasks.map((currentTask,index) => {
+            return (<li key={index}>{currentTask}</li>);
+          })}        
+        </ul>
+      </div>
     </div>
   );
 }
